dcc63e6c1029f79332650f7be60a3feb9f06e752
Andrés K. Aquin <andruuus123@gmail.com>
2021-06-12 02:32:46 -0400

    fibonacci

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 //1.- Fibonacci
 function Fibonacci()
 {
-    var n = document.getElementById("1");
+    let form = document.getElementById("formulario");
+    let num = form.elements['fibonacci'];
     let a=0, b=1,c,s=1;
-    for (let index = 0; index < n; index++) {
-        
+    alert(a, b);
+    for (let index = 3; index <= num; index++) {
+        c=a+b;
+        alert(c);
+        s = s+c;
+        a = b;
+        b = c;
     }
 }
