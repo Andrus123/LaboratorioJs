f25d8315a0eacf4842af7ab8a150ac027affb091
Andrés K. Aquin <andruuus123@gmail.com>
2021-06-12 16:40:06 -0400

    Intentando otra vez

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 //Generar las siguientes series, para ello Utilizar Formularios y Javascript
 //1.- Fibonacci
-function Fibonacci()
-{
-    let form = document.getElementById("formulario");
-    let num = form.elements['fibonacci'];
-    let a=0, b=1,c,s=1;
-    alert(a, b);
-    for (let index = 3; index <= num; index++) {
-        c=a+b;
-        alert(c);
-        s = s+c;
-        a = b;
-        b = c;
-    }
-}
+const fibonacci = document.getElementById("fibonacci")
