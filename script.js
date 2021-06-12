10d4214dcabda10787c9c66f94dc24e4d0b87757
Andrés K. Aquin <andruuus123@gmail.com>
2021-06-12 01:49:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Generar las siguientes series, para ello Utilizar Formularios y Javascript
+//1.- Fibonacci
+function Fibonacci()
+{
+    var n = document.getElementById("1");
+    let a=0, b=1,c,s=1;
+    for (let index = 0; index < n; index++) {
+        
+    }
+}
