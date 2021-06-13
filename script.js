a8fef0572e1c1a16e3f610329bb5b1547118e30a
Andrés K. Aquin <andruuus123@gmail.com>
2021-06-13 18:27:44 -0400

    Casi por terminar maquetacion

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //Generar las siguientes series, para ello Utilizar Formularios y Javascript
 //1.- Fibonacci
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal){
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+function closeModal(modal){
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+
 
 function fibo(){
-    const numero = document.getElementById("fibonacci");
-    alert(numero.value); 
+    const numero = document.getElementById("fibonacci"); 
+    document.getElementById("modal-body").innerHTML = numero.value;
 }
