84f2c37bb5e48240c8a684edfe16a3831714495f
Andrés K. Aquin <andruuus123@gmail.com>
2021-06-13 02:53:12 -0400

    buttons

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //Generar las siguientes series, para ello Utilizar Formularios y Javascript
 //1.- Fibonacci
-    const fibonacci = document.getElementById("fibonacci");
-    const form = document.getElementById("formulario");
 
-    form.addEventListener("submit", e=>{
-        alert("Hola");
-    })
+function fibo(){
+    const numero = document.getElementById("fibonacci");
+    alert(numero.value); 
+}
